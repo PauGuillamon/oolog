36b8886ea578ddf2104b6033ca18d078f42b5167
Pau Guillamon <pauguillamonjaime@gmail.com>
2018-05-02 19:30:48 +0200

    Adds unit tests to Log class

--- test/oologTest.cpp
+++ test/oologTest.cpp
@@ -22,11 +22,57 @@ class MockPrinter : public oolog::FakePrinter {
 
 
 
-TEST_F(LogUnitTest, Test_CheckSameLogLevelIsLogged) {
+TEST_F(LogUnitTest, Test_CheckSameLevelIsLogged) {
 	std::shared_ptr<MockPrinter> logPrinter = std::make_shared<MockPrinter>();
 	oolog::Log log(logPrinter, oolog::LogLevel::Warning);
 
 	EXPECT_CALL(*logPrinter.get(), PrintLog).Times(Exactly(1));
+
+	log.Warning("");
+}
+
+
+
+TEST_F(LogUnitTest, Test_CheckHigherLevelIsLogged) {
+	std::shared_ptr<MockPrinter> logPrinter = std::make_shared<MockPrinter>();
+	oolog::Log log(logPrinter, oolog::LogLevel::Warning);
+
+	EXPECT_CALL(*logPrinter.get(), PrintLog).Times(Exactly(1));
+
+	log.Error("");
+}
+
+
+
+TEST_F(LogUnitTest, Test_CheckLowerLevelIsNotLogged) {
+	std::shared_ptr<MockPrinter> logPrinter = std::make_shared<MockPrinter>();
+	oolog::Log log(logPrinter, oolog::LogLevel::Warning);
+
+	EXPECT_CALL(*logPrinter.get(), PrintLog).Times(Exactly(0));
+
+	log.Verbose("");
+}
+
+
+
+TEST_F(LogUnitTest, Test_CheckErrorIsLoggedAfterChangingLevel) {
+	std::shared_ptr<MockPrinter> logPrinter = std::make_shared<MockPrinter>();
+	oolog::Log log(logPrinter, oolog::LogLevel::Warning);
+
+        log.SetLogLevel(oolog::LogLevel::Error);
+	EXPECT_CALL(*logPrinter.get(), PrintLog).Times(Exactly(0));
+
+	log.Error("");
+}
+
+
+
+TEST_F(LogUnitTest, Test_CheckWarningIsNotLoggedAfterChangingLevel) {
+	std::shared_ptr<MockPrinter> logPrinter = std::make_shared<MockPrinter>();
+	oolog::Log log(logPrinter, oolog::LogLevel::Warning);
+
+        log.SetLogLevel(oolog::LogLevel::Error);
+	EXPECT_CALL(*logPrinter.get(), PrintLog).Times(Exactly(0));
 
 	log.Warning("");
 }
