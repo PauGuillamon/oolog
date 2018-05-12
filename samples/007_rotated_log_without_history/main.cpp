1f4ade3cdb493a2a37012f065751f824d65350eb
Pau Guillamon <pauguillamonjaime@gmail.com>
2018-05-12 12:08:49 +0200

    Fixes samples 7 and 8 after RotatedFIle refactor.

--- samples/007_rotated_log_without_history/main.cpp
+++ samples/007_rotated_log_without_history/main.cpp
@@ -12,12 +12,12 @@ int main(){
         std::make_shared<oolog::printers::decorators::Level>(
         std::make_shared<oolog::printers::decorators::Timestamp>(
         std::make_shared<oolog::printers::decorators::NewLine>(
-		std::make_shared<oolog::printers::RotatedFile>("output_sample_007.log", 94))));
+		std::make_shared<oolog::printers::RotatedFile>("output_sample_007.log", 94, 0))));
 
     oolog::Log log(logPrinter, oolog::LogLevel::Verbose);
 
 	log.Info("Sample 007 - logging to a file that will be rotated ------------------");
-	log.Info("hello rotated log!");
+	log.Info("The previous log won't be in the log after the execution");
 	log.Warning("bye rotated log!");
 
     return 0;
--- samples/008_rotated_log_with_history/main.cpp
+++ samples/008_rotated_log_with_history/main.cpp
@@ -1,6 +1,6 @@
 
 #include "oolog.h"
-#include "printers/RotatedFileWithHistory.h"
+#include "printers/RotatedFile.h"
 #include "printers/decorators/Level.h"
 #include "printers/decorators/NewLine.h"
 #include "printers/decorators/Timestamp.h"
@@ -12,15 +12,17 @@ int main(){
         std::make_shared<oolog::printers::decorators::Level>(
         std::make_shared<oolog::printers::decorators::Timestamp>(
         std::make_shared<oolog::printers::decorators::NewLine>(
-		std::make_shared<oolog::printers::RotatedFileWithHistory>("output_sample_008.log", 20, 3))));
+		std::make_shared<oolog::printers::RotatedFile>("output_sample_008.log", 40, 3))));
 
     oolog::Log log(logPrinter, oolog::LogLevel::Verbose);
 
 	log.Info("Sample 008 - logging to a file that will be rotated with history ------------------");
-	for(int i = 0; i < 10; i++) {
-		log.Info(i, " meh");
-	}
-	log.Warning("bye split log!");
+	int i = 1;
+	log.Info(i++, " This log will be lost after rotating the oldest log file");
+	log.Info(i++, " This log will end in output_sample_008.log.3");
+	log.Info(i++, " This log will end in output_sample_008.log.2");
+	log.Info(i++, " This log will end in output_sample_008.log.1");
+	log.Info("bye rotated log!");
 
     return 0;
 }
