014de2f8e4209d24509c3f3030562b7b4bacf2ae
Pau Guillamon <pauguillamonjaime@gmail.com>
2018-05-14 22:55:42 +0200

    Adds RotatedFileTest

---
+++ test/RotatedFileTest.cpp
@@ -0,0 +1,23 @@
+
+
+#include "printers/RotatedFile.h"
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using ::testing::Exactly;
+
+class RotatedFileTest : public testing::Test {
+	protected:
+		virtual void SetUp() {}
+		virtual void TearDown() {}
+};
+
+
+
+
+
+
+TEST_F(RotatedFileTest, Test_Check) {
+}
+
